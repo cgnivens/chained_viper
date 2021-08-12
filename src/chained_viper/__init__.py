751b52ca7f61fbe1612e7a6058deb91831678a6d
Cory Nivens <cgn7634@gmail.com>
2021-08-12 18:15:02 -0400

    Fixed circular imports with ridiculous import hack

--- src/chained_viper/__init__.py
+++ src/chained_viper/__init__.py
@@ -1,1 +1,16 @@
-from .main import Iter, Option, Result
+from .result import Result
+from .option import Option
+from .iterator import Iter
+
+from . import iterator
+from . import result
+from . import option
+
+iterator.Result = Result
+option.Result = Result
+
+option.Iter = Iter
+result.Iter = Iter
+
+iterator.Option = Option
+result.Option = Option
